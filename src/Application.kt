ef6f9e31a47bffc135b50ebe5589f987c8b4d4fe
Erik Huizinga <erik.huizinga@nedap.com>
2019-12-06 22:16:08 +0100

    🎨 Clean up

--- src/Application.kt
+++ src/Application.kt
@@ -1,9 +1,7 @@
 package io.github.bobvanderlinden
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.client.*
+import io.ktor.application.Application
+import io.ktor.application.install
 import io.ktor.features.DefaultHeaders
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -11,8 +9,5 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    install(DefaultHeaders)
-//    val client = HttpClient() {
-//    }
 }
 
